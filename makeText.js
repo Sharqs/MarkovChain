a4568c68e069acbe0c98ec645d5309b81cc9abff
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-04-30 11:23:54 -0700

    done twas easy

--- makeText.js
+++ makeText.js
@@ -1,2 +1,47 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios')
+const markov = require('./markov')
+
+function file(path){
+    let data = fs.readFileSync(path, 'utf8', function(err, data){
+        if (err) {
+            console.log("Error failed to read File.")
+            process.exit(1)
+        } else{
+            console.log(`Printing contents of the ${data}`)
+        }
+    })
+    return data
+}
+
+function url(url) {
+    try {
+      let data = axios.get(url)
+        .then(response =>  response.data)
+      return data
+    } catch (err) {
+      console.error(`Error fetching ${url}: ${err}`);
+      process.exit(1);
+    }
+}
+  
+var path = process.argv[3]
+var fn = process.argv[2]
+
+async function splitter(max){
+    let data
+    if (fn === 'file') {
+        data = file(path);
+        let mm = new markov.MarkovMachine(data)
+        console.log(mm.makeText(max))
+    } else {
+        data = await url(path);
+        let mm = new markov.MarkovMachine(data)
+        let text = mm.makeText(max)
+        console.log(text +'**********'+ text.split(' ').length)
+    }
+}
+splitter(1000)
 
--- markov.js
+++ markov.js
@@ -18,6 +18,15 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
+    let result = {}
+    for (let i = 0; i<this.words.length; i++){
+      if (result[this.words[i]]){
+        result[this.words[i]].push(this.words[i+1])
+      }
+      else
+        result[this.words[i]] = [this.words[i+1]]
+    }
+    this.result = result
   }
 
 
@@ -25,5 +34,26 @@ class MarkovMachine {
 
   makeText(numWords = 100) {
     // TODO
+    let wordGen = []
+    function random(max){
+      return Math.floor(Math.random() * (max + 1))
+    }
+    let resultLength = Object.keys(this.result).length-1
+    let resultKeys = Object.keys(this.result)
+    wordGen.push(resultKeys[random(resultLength)])
+    for (let i = 0; i<numWords-1; i++){
+      let lastWord = this.result[wordGen[i]]
+      let ran = random(lastWord.length-1)
+      if (lastWord[ran] === undefined){
+        wordGen.push('.')
+        break
+      }
+      wordGen.push(lastWord[ran])
+    }
+    return wordGen.join(' ')
   }
+}
+
+module.exports = {
+  MarkovMachine
 }
